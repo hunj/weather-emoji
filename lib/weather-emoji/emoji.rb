e208b340d9135d8d25e68e4cb1aa9238bdd48872
Hun Jae Lee <hunjael@gmail.com>
2017-07-08 17:59:39 -0400

    :alien: ayy lmao here comes the emoji definitions

--- lib/weather-emoji/emoji.rb
+++ lib/weather-emoji/emoji.rb
@@ -1,28 +1,46 @@
+class Integer
+  # corresponding emoji for moon phase
+  def to_moonphase
+    case self
+    when 1
+      "\u{1F311}" # new moon
+    when 2
+      "\u{1F312}" # waxing crescent moon
+    when 3
+      "\u{1F313}" # first quarter moon
+    when 4
+      "\u{1F314}" # waxing gibbous moon
+    when 5
+      "\u{1F315}" # full moon
+    when 6
+      "\u{1F316}" # waning gibbous moon
+    when 7
+      "\u{1F317}" # last quarter moon
+    when 8
+      "\u{1F318}" # waning crescent moon
+    end
+  end
 
-module WeatherEmoji
   # corresponding emoji for one single weather code.
-  # TODO separate this into a "dictionary"
-  def self.emoji_for weather_code
-    case weather_code
-    when (312..314), 321, (502..504), 511, (520..522), 531, 906
-      # heavy_rain
-    when (300..311), 500, 501
-      # light_rain.sample
-    when (600..602), 611, 612, 615, 616, (620..622), 903
-      # snow
-    when 701, 711, 721, 731, 741, 751, 761, 905, (952..958)
-      # fog_wind
-    when 762, 771, 781, (900..902), 962
-      # extreme.sample
-    when 800, 801, 904, 951
-      # sunny.sample
-    when 802, 803, 950
-      # partly_cloudy.sample
+  # TODO verify each weather code to emoji conversion is accurate
+  def to_emoji
+    case self
+    when (200...300)
+      "\u{26A1}" # thunderstorm
+    when (300...400)
+      "\u{1F326}" # light rain (drizzle)
+    when (500...600)
+      "\u{2614}" # rain
+    when 800
+      "\u{1F31E}" # clear sky
+    when 801
+      "\u{26C5}" # few clouds
+    when 802
+      "\u{1F324}" # scattered clouds
+    when 803
+      "\u{1F325}" # broken clouds
     when 804
-      # cloudy.sample
-    when (200..202), (210..212), 221, (230..232), (959..961)
-      # thunder.sample
+      "\u{2601}" # overcast clouds
     end
   end
 end
-
