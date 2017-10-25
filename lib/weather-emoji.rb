b484188514781dac1f5422e051ef10e8ef59e09d
Hun Jae Lee <hunjael@gmail.com>
2017-10-25 16:29:38 -0400

    :sparkles: edit wrapper corresponding to optparse

--- lib/weather-emoji.rb
+++ lib/weather-emoji.rb
@@ -5,10 +5,12 @@ require_relative 'weather-emoji/api'
 require_relative 'weather-emoji/emoji'
 
 module WeatherEmoji
-  def self.weather host=nil
-    coord = get_coordinates host
-    result = get_weather_for_coordinates coord, 'c'
+  # the "wrapper" for everything.
+  def self.weather options
+    coord = get_coordinates options[:host]
+    result = get_weather_for_coordinates coord, options
     data = consume_weather_data result
-    WeatherEmoji::stringify data
+    puts WeatherEmoji::stringify data
+    exit
   end
 end
